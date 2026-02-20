338975bfa745048dc727963e904e4815cd26f5c3
deniz41-justacceptmyusername <nsylssylst@gmail.com>
2026-02-20 10:09:48 +0300

    First commit

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,46 @@
+package frc.robot;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.subsystems.SwerveSubsystem;
+import swervelib.SwerveInputStream;
+
+public class RobotContainer {
+  // Subsystem tanımı
+  private final SwerveSubsystem drivebase = new SwerveSubsystem();
+
+  // Xbox Kontrolcüsü (Port 0)
+  private final CommandXboxController m_driverController =
+      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+
+  public RobotContainer() {
+    configureBindings();
+
+    // Sürüş Giriş Akışı (Input Stream) Yapılandırması
+    SwerveInputStream driveAngularVelocity = SwerveInputStream.of(drivebase.getSwerveDrive(),
+        () -> -m_driverController.getLeftY(),
+        () -> -m_driverController.getLeftX())
+        .withControllerRotationAxis(() -> -m_driverController.getRightX())
+        .deadband(OperatorConstants.DEADBAND)
+        .scaleTranslation(0.8) // Hızı %80'e sınırlar, güvenli sürüş sağlar
+        .allianceRelativeControl(true);
+
+    // Varsayılan komut olarak sürüşü ata
+    drivebase.setDefaultCommand(drivebase.driveFieldOriented(driveAngularVelocity));
+  }
+
+  private void configureBindings() {
+    // Start butonu veya B butonu Gyro'yu sıfırlar (Robotun baktığı yer ileri olur)
+    m_driverController.start().onTrue(new InstantCommand(drivebase::zeroGyro));
+    m_driverController.b().onTrue(new InstantCommand(drivebase::zeroGyro));
+  }
+
+  public Command getAutonomousCommand() {
+    // Otonom komutu buraya gelecek
+    return null; 
+  }
+} 
---
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -0,0 +1,60 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import java.io.File;
+import java.util.function.Supplier;
+import com.studica.frc.AHRS;
+import edu.wpi.first.wpilibj.Filesystem;
+import swervelib.parser.SwerveParser;
+import swervelib.telemetry.SwerveDriveTelemetry;
+import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
+import swervelib.SwerveDrive;
+import swervelib.math.SwerveMath;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.util.Units;
+
+public class SwerveSubsystem extends SubsystemBase {
+  private final File directory = new File(Filesystem.getDeployDirectory(), "swerve");
+  private final SwerveDrive swerveDrive;
+
+  public SwerveSubsystem() {
+    // Dişli oranlarına göre çevrim katsayıları
+    double angleConversionFactor = SwerveMath.calculateDegreesPerSteeringRotation(12.8, 1);
+    double driveConversionFactor = SwerveMath.calculateMetersPerRotation(Units.inchesToMeters(4), 6.75, 1);
+
+    SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
+
+    try {
+      swerveDrive = new SwerveParser(directory).createSwerveDrive(Constants.maxSpeed, angleConversionFactor, driveConversionFactor);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+
+    // Absolute encoder olmadığı için kritik ayarlar
+    swerveDrive.setHeadingCorrection(false); 
+    swerveDrive.setCosineCompensator(false); // Mutlak encoder yoksa bu false olmalı
+  }
+
+  // RobotContainer'ın kullanacağı ana sürüş metodu
+  public Command driveFieldOriented(Supplier<ChassisSpeeds> velocity) {
+    return run(() -> {
+      swerveDrive.driveFieldOriented(velocity.get());
+    });
+  }
+
+  public SwerveDrive getSwerveDrive() {
+    return swerveDrive;
+  }
+
+  public void zeroGyro() {
+    swerveDrive.zeroGyro();
+  }
+
+  @Override
+  public void periodic() {
+    // Odometriyi her döngüde günceller
+  }
+}
