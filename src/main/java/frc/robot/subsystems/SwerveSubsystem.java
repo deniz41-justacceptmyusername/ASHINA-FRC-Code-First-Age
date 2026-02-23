c6036233a5e3bb1ec2f559de64edaf95df141e21
deniz41-justacceptmyusername <nsylssylst@gmail.com>
2026-02-23 10:53:58 +0300

    cancoder test

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -3,45 +3,68 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+
 import java.io.File;
 import java.util.function.Supplier;
-import com.studica.frc.AHRS;
+
+
+import com.ctre.phoenix6.swerve.SwerveModule;
+
 import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.Timer;
+
 import swervelib.parser.SwerveParser;
 import swervelib.telemetry.SwerveDriveTelemetry;
 import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
 import swervelib.SwerveDrive;
 import swervelib.math.SwerveMath;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.util.Units;
+import com.ctre.phoenix6.hardware.CANcoder;
+
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 
 public class SwerveSubsystem extends SubsystemBase {
-  private final File directory = new File(Filesystem.getDeployDirectory(), "swerve");
+
+  private final File directory =
+      new File(Filesystem.getDeployDirectory(), "swerve");
   private final SwerveDrive swerveDrive;
+  
+public SwerveSubsystem() {
+  this(new File(Filesystem.getDeployDirectory(), "swerve"));
+}
+  public SwerveSubsystem(File directory) {
 
-  public SwerveSubsystem() {
-    // Dişli oranlarına göre çevrim katsayıları
-    double angleConversionFactor = SwerveMath.calculateDegreesPerSteeringRotation(12.8, 1);
-    double driveConversionFactor = SwerveMath.calculateMetersPerRotation(Units.inchesToMeters(4), 6.75, 1);
-
+    double angleConversationFactor = SwerveMath.calculateDegreesPerSteeringRotation(26, 1);
+    double driveConversationFactor = SwerveMath.calculateMetersPerRotation(Units.inchesToMeters(2), 7.03, 1);
+    
     SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
 
     try {
-      swerveDrive = new SwerveParser(directory).createSwerveDrive(Constants.maxSpeed, angleConversionFactor, driveConversionFactor);
+      swerveDrive =
+          new SwerveParser(directory)
+              .createSwerveDrive(Constants.maxSpeed);
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
-
-    // Absolute encoder olmadığı için kritik ayarlar
     swerveDrive.setHeadingCorrection(false); 
-    swerveDrive.setCosineCompensator(false); // Mutlak encoder yoksa bu false olmalı
+
+    for(swervelib.SwerveModule m : swerveDrive.getModules()) {
+      System.out.println("Module Name: "+m.configuration.name);
+      CANcoder absoluteEncoder = (CANcoder)m.configuration.absoluteEncoder.getAbsoluteEncoder();
+    }
+    // 🔥 NavX kalibrasyon bekleme (çok önemli)
+    Timer.delay(4.0);
+
+    // 🔥 Robotun başlangıç yönünü ileri kabul et
+    swerveDrive.zeroGyro();
   }
 
-  // RobotContainer'ın kullanacağı ana sürüş metodu
-  public Command driveFieldOriented(Supplier<ChassisSpeeds> velocity) {
+  // Field oriented sürüş (gyro configten geliyor)
+  public Command driveFieldOriented(
+      Supplier<ChassisSpeeds> speeds) {
+
     return run(() -> {
-      swerveDrive.driveFieldOriented(velocity.get());
+      swerveDrive.driveFieldOriented(speeds.get());
     });
   }
 
@@ -51,10 +74,5 @@ public class SwerveSubsystem extends SubsystemBase {
 
   public void zeroGyro() {
     swerveDrive.zeroGyro();
-  }
-
-  @Override
-  public void periodic() {
-    // Odometriyi her döngüde günceller
   }
 }
