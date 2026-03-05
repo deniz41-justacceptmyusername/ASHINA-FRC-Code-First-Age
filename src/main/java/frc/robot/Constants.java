c3c5fdf750c3bd3486bfccefe56af90b65f47b71
deniz41-justacceptmyusername <nsylssylst@gmail.com>
2026-03-05 10:37:51 +0300

    cancoders and gyro has been added

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -17,8 +17,8 @@ import edu.wpi.first.math.util.Units;
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
-    
+
     public static final double DEADBAND = 0.05;
   }
-  public static final double maxSpeed = Units.feetToMeters(20);
+  public static final double maxSpeed = Units.feetToMeters(5);
 }
